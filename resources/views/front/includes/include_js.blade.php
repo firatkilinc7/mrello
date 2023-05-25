0fbfa652b45e4ee75a2fd84ed5d7621e35c95da3
Mert Cevik <cvkmert0@gmail.com>
2023-05-25 13:54:26 +0300

    Front
    Added Js
    Include

---
+++ resources/views/front/includes/include_js.blade.php
@@ -0,0 +1,12 @@
+<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js"></script>
+    <script
+      src="https://cdnjs.cloudflare.com/ajax/libs/dragula/3.6.6/dragula.min.js"
+      integrity="sha512-MrA7WH8h42LMq8GWxQGmWjrtalBjrfIzCQ+i2EZA26cZ7OBiBd/Uct5S3NP9IBqKx5b+MMNH1PhzTsk6J9nPQQ=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    ></script>
+    <script src="{{asset('assets/front/js/createCard.js')}}"></script>
+    <script src="{{asset('assets/front/js/main.js')}}"></script>
+    <script src="{{asset('assets/front/js/responsive.js')}}"></script>
+    <script src="{{asset('assets/front/js/usersGenerate.js')}}"></script>
