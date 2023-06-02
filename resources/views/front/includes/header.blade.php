678fc585b460baafc08dc43316aff477de5e7617
Mert Cevik <cvkmert0@gmail.com>
2023-06-02 23:54:32 +0300

    Update Header
    Update Header

--- resources/views/front/includes/header.blade.php
+++ resources/views/front/includes/header.blade.php
@@ -1,7 +1,7 @@
 <header class="header-area">
       <div class="header__wrapper">
         <div class="header__board">
-          <p class="header__board-txt">Mert Cevik's Board</p>
+          <p class="header__board-txt">Board</p>
         </div>
         <a href="" class="header__logo">
           <img src="{{asset("assets/front/images/logo.png")}}" alt="logo" class="header__logo" />
